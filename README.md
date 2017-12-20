35734e6a53212a77c0d55f4d4668ab83726a247f
Mark Cristobal <vhin0210@gmail.com>
2017-12-20 10:29:12 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# avatars.adorable.io-php
+
+This is a simple PHP library that will get avatars from avatars.adorable.io.
+
+**Install via Composer**
+```
+composer require vhin0210/avatars.adorable.io-php
+```
---
+++ src/AvatarsAdorableIO.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AvatarsAdorableIOPHP;
+
+class AvatarsAdorableIO {
+    const API_DOMAIN = 'https://api.adorable.io/';
+    const API_ENDPOINT = 'avatars/';
+
+    const API_RESOURCE_AUTO_GENERATE = '{size}/{input}';
+    const API_RESOURCE_LIST = 'list';
+    const API_RESOURCE_FACE = 'face/{eyes}/{nose}/{mouth}/{color}';
+
+
+}
